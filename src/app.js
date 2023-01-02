4f8439b1e512649e721c636c5564cc5b91d54592
Pranjal Kesarwani <89082511+PranjalKesarwani@users.noreply.github.com>
2023-01-02 17:18:02 +0530

    creation and insertion of doc

--- src/app.js
+++ src/app.js
@@ -31,3 +31,32 @@ A mongoose model is a wrapper on the mongoose schema. A mongoose schema defines 
 
 //now we are defining the class, it is model
 const Playlist = new mongoose.model("Playlist", playlistSchema); //Playlist is in Pascalcase  and inside parenthesis P should be capital and when you will see in compass this Playlist will be shown as playlists
+
+
+//creation and insertion of document
+
+
+const createDocument = async ()=>{
+    try {
+        const reactPlaylist = new Playlist({
+            name: "Node JS",
+            ctype: "Back-End",
+            videos: 50,
+            author: "Thapa Technical",
+            active: true,
+           
+        })
+
+        const result=await reactPlaylist.save();
+
+        console.log(result);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+createDocument();
+
+
+// const result=await reactPlaylist.save(); //ye .save() method promise return krta hai so isme async await use kr sakte ho, maine mongoose connect krte waqt use ni kiya but ab harr jagah use krege
